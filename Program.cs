fec829603306d6c61437f275dbe1560c62e7c79d
Harshil Arora <harshil.arora03@gmail.com>
2026-08-21 22:27:31 +0400

    basic operation detection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello User! Welcome to Sheix!");
+﻿Console.WriteLine("Hello User! Welcome to Sheix!");
+
+// Basic Operation detection program..
+
+
+string operation = Console.ReadLine();
+
+if (operation=="+")
+{
+    Console.WriteLine("You chose addition! (+)");
+}
+
+if (operation == "-")
+{
+    Console.WriteLine("You chose subtraction! (-)");
+}
+
+if (operation == "/")
+{
+    Console.WriteLine("You chose division! (/)");
+}
+
+if (operation == "*")
+{
+    Console.WriteLine("You chose multiplication! (*)");
+}
